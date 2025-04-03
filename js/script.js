0b7946ede655c1698558078e6d7afd981d648e69
sebastien c. <schatel.pro@pm.me>
2025-04-03 14:57:26 +0200

    (init) add tag JS in HTML and test JS on form

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
 //--------------------------//
 //--------- SCRIPT ---------//
-// COMMENTAIRES
+//Conceptualisation
+// 1er Après remplissage du formulaire et en cliquant sur suivant, le block du formulaire disparait et
+// un autre réapparait avec :
+//  Le choix de l'hotel (2 hôtels)
+//  Prix (2 prix)
+//  Nombre de personnes
+//  Date depart et arrivee
+//
 //--------------------------//
+//--- Selecteurs
+//formulaire
+let form  = document.getElementById("formulaires");
+//bouton formulaire
+let btnform = document.getElementById("sendForm");
+//--------------------------//
+
+if (form){
+    console.log("OK FORM !")
+}
+
+if (btnform){
+    console.log("OK BTN !")
+}
